06a1159035c932a75bf893bbccb9d2ce635dd945
EzequielTecsid <ezequiel@tecsid.com>
2024-09-20 17:08:12 -0300

    implementacion useReducer

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,12 +1,14 @@
+import { Dispatch } from "react";
 import { formatCurrency } from "../helpers";
-import { MenuItem, OrderItem } from "../types";
+import { OrderItem } from "../types";
+import { OrderActions } from "../reducers/order-reducer";
 
 type OrderContentsProps = {
   order: OrderItem[]
-  removeItem: (id: MenuItem['id']) => void
+  dispatch: Dispatch<OrderActions>
 };
 
-export const OrderContents = ({ order, removeItem }: OrderContentsProps) => {
+export const OrderContents = ({ order, dispatch }: OrderContentsProps) => {
   return (
     <div>
       <h2 className="font-black text-4xl">Consumo</h2>
@@ -27,7 +29,7 @@ export const OrderContents = ({ order, removeItem }: OrderContentsProps) => {
               </div>
               <button 
                 className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
-                onClick={() => removeItem(item.id)}
+                onClick={() => dispatch({type:'remove-item', payload: {id: item.id}})}
                 >
                 X
               </button>
